f6411c008e84135a76b3ed236819f99505ca9c31
raphael <raphael.hermans@hics.be>
2025-08-28 19:46:18 +0200

    added train_network and mean_squared_errors

--- src/rnet/data.rs
+++ src/rnet/data.rs
@@ -14,15 +14,26 @@ pub struct Dataset {
 impl Dataset {
     /// Creates a new Dataset
     pub fn new(inputs: Vec<Array1<f64>>, targets: Vec<Array1<f64>>) -> Self {
+        assert!(inputs.len() == targets.len());
         Dataset { inputs, targets, randseed: RANDSEED }
     }
 
-    /// Returns a random iterator over the dataset
+    /// Returns a random iterator over the dataset with (input, target) pairs
     pub fn random_iterator(&self) -> impl Iterator<Item = (&Array1<f64>, &Array1<f64>)> {
         let mut rng = StdRng::seed_from_u64(self.randseed);
         let mut indices: Vec<usize> = (0..self.inputs.len()).collect();
         indices.shuffle(&mut rng);
         indices.into_iter().map(move |i| (&self.inputs[i], &self.targets[i]))
+    }
+
+    /// Returns an iterator over the dataset with (input, target) pairs
+    pub fn iterator(&self) -> impl Iterator<Item = (&Array1<f64>, &Array1<f64>)> {
+        self.inputs.iter().zip(&self.targets)
+    }
+
+    /// Returns the number of samples in the dataset
+    pub fn len(&self) -> usize {
+        self.inputs.len()
     }
 
     /// Sets the random seed for the dataset
--- src/rnet/train.rs
+++ src/rnet/train.rs
@@ -1,10 +1,23 @@
+use std::vec;
+
 use crate::rnet::data::Dataset;
 use crate::rnet::network::Network;
 use ndarray::Array1;
 
-pub fn train_network(net: &mut Network, data: Dataset, batch_size: usize, epochs: usize, learn_rate: f64) {
+/// Trains the given neural network using the provided dataset, batch size,
+/// number of epochs, and learning rate.
+pub fn train_network(
+    net: &mut Network,
+    data: Dataset,
+    batch_size: usize,
+    epochs: usize,
+    learn_rate: f64,
+) {
     for _ in 0..epochs {
-        let (mut inputs, mut targets) = (Vec::<&Array1<f64>>::new(), Vec::<&Array1<f64>>::new());
+        let (mut inputs, mut targets) = (
+            Vec::<&Array1<f64>>::new(),
+            Vec::<&Array1<f64>>::new()
+        );
         for (input, target) in data.random_iterator() {
             inputs.push(input);
             targets.push(target);
@@ -16,5 +29,16 @@ pub fn train_network(net: &mut Network, data: Dataset, batch_size: usize, epochs
         }
         net.backwards_propagation(learn_rate, &inputs, &targets);
     }
+}
 
+/// Computes the mean squared errors between the network's predictions and the target outputs.
+pub fn mean_squared_errors(network: &Network, dataset: &Dataset) -> Vec<f64> {
+    let mut squared_errors = vec![0.0; dataset.len()];
+    for (input, target) in dataset.iterator() {
+        let test_output = network.forward_prop(&input);
+        squared_errors.push(
+            (test_output - target).mapv(|x| x.powi(2) / target.len() as f64).sum()
+        );
+    }
+    squared_errors
 }
