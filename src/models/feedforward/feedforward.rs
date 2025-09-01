68d3f304d2e3bc51faffbeafd09f74a69d2b5ceb
raphael <raphael.hermans@hics.be>
2025-09-01 18:38:37 +0200

    added test for FeedForward

--- src/models/feedforward/feedforward.rs
+++ src/models/feedforward/feedforward.rs
@@ -1,27 +1,41 @@
 use ndarray::{Array1, Array2};
 use rand::{rngs::StdRng, Rng, SeedableRng};
-use crate::models::{feedforward::hidden_layer::HiddenLayer, InputLayer, OutputLayer};
+use crate::{loss::Loss, models::{feedforward::hidden_layer::HiddenLayer, InputLayer, OutputLayer}};
 
 const RANDSEED: u64 = 0;
+const DEFAULT_LEARNING_RATE: f32 = 0.1;
+const DEFAULT_BATCH_SIZE: usize = 32;
+const DEFAULT_EPOCHS: usize = 10;
 
 /// A representation of a feedforward neural network
 struct FeedForward {
     input_layer: InputLayer,
     hidden_layers: Vec<HiddenLayer>,
     output_layer: OutputLayer,
+    loss: Box<dyn Loss>,
+    
+    // Learning parameters
+    learning_rate: f32,
+    batch_size: usize,
+    epochs: usize,
 }
 
 impl FeedForward {
-    /// Creates a new FeedForward model with the given layer
+    /// Creates a new FeedForward model with the given layers and loss function
     pub fn new(
         input_layer: InputLayer,
         hidden_layers: Vec<HiddenLayer>,
         output_layer: OutputLayer,
+        loss: Box<dyn Loss>,
     ) -> Self {
         let mut instance = Self {
             input_layer,
             hidden_layers,
             output_layer,
+            loss,
+            learning_rate: DEFAULT_LEARNING_RATE,
+            batch_size: DEFAULT_BATCH_SIZE,
+            epochs: DEFAULT_EPOCHS,
         };
         instance.initialize_weights_and_biases();
         instance
@@ -41,6 +55,7 @@ impl FeedForward {
         }
 
         // Initialize the output layer
+        self.output_layer.biases = Some(Self::rand_bias(self.output_layer.dim));
         if self.hidden_layers.is_empty() {
             self.output_layer.weights = Some(Self::rand_weights(
                 self.output_layer.dim,
@@ -64,5 +79,39 @@ impl FeedForward {
     fn rand_weights(rows: usize, cols: usize) -> Array2<f32> {
         let mut rng = StdRng::seed_from_u64(RANDSEED);
         Array2::from_shape_fn((rows, cols), |_| rng.random_range(-1.0..1.0))
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::{activation::{Linear}, loss::CrossEntropy};
+    use super::*;
+
+    #[test]
+    fn test_feedforward_initialization() {
+        let input_layer = InputLayer::new(3);
+        let hidden_layers = vec![
+            HiddenLayer::new(4, Box::new(Linear)),
+            HiddenLayer::new(5, Box::new(Linear)),
+        ];
+        let output_layer = OutputLayer::new(2, Box::new(Linear));
+        let loss = Box::new(CrossEntropy);
+
+        let model = FeedForward::new(
+            input_layer,
+            hidden_layers,
+            output_layer,
+            loss,
+        );
+
+        // Biases
+        assert_eq!(model.hidden_layers[0].biases.as_ref().unwrap().len(), 4);
+        assert_eq!(model.hidden_layers[1].biases.as_ref().unwrap().len(), 5);
+        assert_eq!(model.output_layer.biases.as_ref().unwrap().len(), 2);
+        
+        // Weights
+        assert_eq!(model.hidden_layers[0].weights.as_ref().unwrap().shape(), &[4, 3]);
+        assert_eq!(model.hidden_layers[1].weights.as_ref().unwrap().shape(), &[5, 4]);
+        assert_eq!(model.output_layer.weights.as_ref().unwrap().shape(), &[2, 5]);
     }
 }
